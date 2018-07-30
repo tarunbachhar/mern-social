d4331bde70769fefdc4787dbc6746cc4212382ff
tarunbachhar <tarunb420@gmail.com>
2018-07-30 15:46:42 +0530

    second submission

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 // mongodb://tarunb:tarunb7827@ds257851.mlab.com:57851/mernsocial
 
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/mernsocial"
+  mongoURI: "mongodb://localhost:27017/mernsocial",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
@@ -7,6 +8,9 @@ const profile = require("./routes/api/profile");
 
 const app = express();
 
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 //db Config
 const db = require("./config/keys").mongoURI;
 
