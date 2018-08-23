87b8b0e9cfae68100a6d5c858402a9e7144a113d
tarunbachhar <tarunb420@gmail.com>
2018-08-23 20:25:25 +0530

    front-end-10

---
+++ client/src/actions/postActions.js
@@ -0,0 +1,99 @@
+import axios from "axios";
+import {
+  GET_ERRORS,
+  ADD_POST,
+  GET_POST,
+  POST_LOADING,
+  GET_POSTS,
+  DELETE_POST
+} from "./types";
+
+//add post
+
+export const addPost = postData => dispatch => {
+  axios
+    .post("/api/posts", postData)
+    .then(res =>
+      dispatch({
+        type: ADD_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//get post
+export const getPosts = () => dispatch => {
+  console.log("helel");
+  dispatch(setPostLoading());
+  axios
+    .get("/api/posts")
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+//delete post
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Add like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Add like
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//set loading state
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  };
+};
