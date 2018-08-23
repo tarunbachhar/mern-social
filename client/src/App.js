a39e29e11c0e9b2c2a699f0798bded9dfd557721
tarunbachhar <tarunb420@gmail.com>
2018-08-23 16:03:53 +0530

    front-end-9

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,8 @@ import "./App.css";
 import { clearCurrentProfile } from "./actions/profileActions";
 import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
+import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 
 //check for token
 if (localStorage.jwtToken) {
@@ -55,6 +57,8 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
