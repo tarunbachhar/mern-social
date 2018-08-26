0574d83cbd28a11db010fbaf99ead0cfc718f4f9
tarunbachhar <tarunb420@gmail.com>
2018-08-26 17:08:40 +0530

    front-end-10-3

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-// mongodb://tarunb:tarunb7827@ds257851.mlab.com:57851/mernsocial
-
-module.exports = {
-  mongoURI: "mongodb://tarunb:tarunb7827@ds257851.mlab.com:57851/mernsocial",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
